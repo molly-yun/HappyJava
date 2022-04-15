1eb56bd0af026eab7d0e056a3d9b6678b63c0928
molly-yun <mollyyun91@gmail.com>
2022-04-15 11:31:03 +0900

    day0409 commit

---
+++ src/day0409/CarExam02.java
@@ -0,0 +1,12 @@
+package day0409;
+
+import day0407.Bus;
+import day0407.Car;
+import day0407.SuperCar;
+
+public class CarExam02 {
+    public static void main(String[] args) {
+        Car c1 = new Car();
+        System.out.println(c1);
+    }
+}
