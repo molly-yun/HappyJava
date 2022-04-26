fcdda2c41b851879e27aa20d739e1468c4b78ed6
molly-yun <mollyyun91@gmail.com>
2022-04-27 03:41:48 +0900

    modify

--- src/day0407/Car.java
+++ src/day0407/Car.java
@@ -4,4 +4,10 @@ public class Car {
     public void run() {
         System.out.println("전륜구동으로 달리다.");
     }
+
+    // toString() 메소드를 오버라이딩하지 않으면 인스턴스의 메모리주소값이 출력되는데 일반적으로 이 값은 쓸모없는 값이기 때문
+    @Override
+    public String toString() {
+        return "자동차!!";
+    }
 }
--- src/day0409/CarExam02.java
+++ src/day0409/CarExam02.java
@@ -7,6 +7,11 @@ import day0407.SuperCar;
 public class CarExam02 {
     public static void main(String[] args) {
         Car c1 = new Car();
-        System.out.println(c1);
+        System.out.println(c1); //println(Object x) => Object로 참조할 수 있는 것은 무엇이든 받을 수 있다.
     }
 }
+
+// 부모타입의 변수로 자식인스턴스를 참조할 수 있다.
+// 조상타입의 변수로 후손인스턴스를 참조할 수 있다.
+// Car c1 = new Bus();
+// Object o1 = new Bus();
