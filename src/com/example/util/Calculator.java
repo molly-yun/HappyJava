a83887120544a832e9943c1bfbb9bf30bc146af5
molly-yun <mollyyun91@gmail.com>
2022-05-11 16:05:11 +0900

    day0405 commit

--- src/com/example/util/Calculator.java
+++ src/com/example/util/Calculator.java
@@ -8,4 +8,10 @@ public class Calculator {
     public int minus(int x, int y) {
         return x - y;
     }
+
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        int value = cal.plus(10, 100);
+        System.out.println(value);
+    }
 }
---
+++ src/com/example/util2/Calculator.java
@@ -0,0 +1,7 @@
+package com.example.util2;
+
+public class Calculator {
+    public int devide(int x, int y) {
+        return x / y;
+    }
+}
